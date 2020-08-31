e9a8a2162e8b60f54d279f9db5b3ff05e6ce17b2
Mohammad <mohammadidehgamwala5@gmail.com>
2020-08-31 13:18:12 +0530

    Updates

---
+++ precache-manifest.ecba734fe082873ba5df8a957ccb2c79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d583b66f3e609b85c20fef4b1ddbd41",
+    "url": "/corona-data/index.html"
+  },
+  {
+    "revision": "6c020ad257f32911b2d1",
+    "url": "/corona-data/static/css/main.7f4dba8a.chunk.css"
+  },
+  {
+    "revision": "3be0c01c8ca12f4f286d",
+    "url": "/corona-data/static/js/2.f8a024c9.chunk.js"
+  },
+  {
+    "revision": "c46ade285a7bd9e73d4de71c9509bd70",
+    "url": "/corona-data/static/js/2.f8a024c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c020ad257f32911b2d1",
+    "url": "/corona-data/static/js/main.220fb126.chunk.js"
+  },
+  {
+    "revision": "43b5a53fb455529533ac",
+    "url": "/corona-data/static/js/runtime-main.9378f08a.js"
+  },
+  {
+    "revision": "c53c344b0d8b7839b3fcc6fdf782c842",
+    "url": "/corona-data/static/media/COVID- 19 (1).c53c344b.png"
+  }
+]);
